f133cecd526f548642bc644cf9f3c30d7e75f4cd
jignyasamishra <iamjignyasa@gmail.com>
2024-08-19 21:39:32 +0530

    server
    Signed-off-by: jignyasamishra <iamjignyasa@gmail.com>

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -1,6 +1,11 @@
 import jwt, datetime , os
 from flask import Flask, request
-from flask_mysqldb import flask_mysqldb
+from flask_mysqldb import MySQL
+
 
 server = Flask(__name__)
 mysql = MySQL(server)
+
+# config
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+print(server.config["MYSQL_HOST"])
